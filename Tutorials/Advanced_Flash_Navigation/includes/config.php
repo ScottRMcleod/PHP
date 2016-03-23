3fa697328520625511c84a30e24e4488a78bd568
Scott Mcleod <smcleod_06@hotmail.com>
2016-03-23 11:30:33 +1100

    Update config.php

--- Tutorials/Advanced_Flash_Navigation/includes/config.php
+++ Tutorials/Advanced_Flash_Navigation/includes/config.php
@@ -2,25 +2,25 @@
 //establishing connection to Database
  function openDB()
  {
-			global $conn, $username,$host,$password,$db;
-				$host = "localhost";
-				$username ="exampleUsername";
-				$password= "examplePassword";
-				$db = "exampleDB";
-					$conn = mysql_connect($host, $username,$password) or die(mysql_error());
-		mysql_select_db($db,$conn) or die(mysql_error());
-		}
-		 function closeDB()
-		{
-			global $conn;
-			mysql_close($conn);
-		}
-		function emailChecker($email)
-		{
-			global $conn, $check_result;
-			$check = "select * from autho_users where subscribe = $subscribe";
-			$check_result = mysql_query($check,$conn)or die(mysql_error());
-		}
+	global $conn, $username,$host,$password,$db;
+	$host = "localhost";
+	$username ="exampleUsername";
+	$password= "examplePassword";
+	$db = "exampleDB";
+	$conn = mysql_connect($host, $username,$password) or die(mysql_error());
+	mysql_select_db($db,$conn) or die(mysql_error());
+}
+function closeDB()
+{
+	global $conn;
+	mysql_close($conn);
+}
+function emailChecker($email)
+{
+	global $conn, $check_result;
+	$check = "select * from autho_users where subscribe = $subscribe";
+	$check_result = mysql_query($check,$conn)or die(mysql_error());
+}
 // format MySQL DATETIME value into a more readable string
 function formatDate($val)
 {
