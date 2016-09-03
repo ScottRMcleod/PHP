54045a3e98c3cac473dd294a5230a412382a2a07
Scott Mcleod <scott.mcleod@smcleodtech.com.au>
2016-09-03 12:04:33 +1000

    Create footer.php

---
+++ Boilerplate/footer.php
@@ -0,0 +1,55 @@
+
+</div>
+</script>
+<div>
+  <footer style='background-color: black!important;'>
+ <div class="container" style='background-color: black!important;'>
+        <div class="row">
+          <div class="col-sm-4">
+            <a a:visited style='color:white' rel='nofollow external' title='Facebook' href='#' target='_blank' class='btn btn-primary btn-circle'><i class='fa fa-facebook'> </i> </a>
+              <a a:visited style='color:white' rel='nofollow external' title='Linkedin' href='#' target='_blank' class='btn btn-primary btn-circle'><i class='fa fa-linkedin'> </i> </a>
+              <a a:visited style='color:white' rel='nofollow external' title='twitter' href='#' target='_blank' class='btn btn-primary btn-circle'><i class='fa fa-twitter'> </i> </a>
+              <a a:visited style='color:white' rel='nofollow external' title='GitHub' href='#' target='_blank' class='btn btn-primary btn-circle'><i class='fa fa-github'> </i> </a>
+              <a a:visited style='color:white' rel='nofollow external' title='Google+' href='#' target='_blank' class='btn btn-primary btn-circle'><i class='fa fa-google-plus'></i></a>
+          <h6><font style='color:white'>Copyright &copy; <?php echo date('Y');?></font></h6>
+          </div><!-- end col-sm-2 -->
+          
+          <div class="col-sm-4">
+            <h6 class='footerh6'>About Us</h6>
+            <p></p>
+          </div><!-- end col-sm-4 -->
+          
+          <div class="col-sm-2">
+            <h6 class='footerh6'>Navigation</h6>
+            <ul class="unstyled">
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Services</a></li>
+              <li><a href="#">Links</a></li>
+              <li><a href="#contacts">Contact</a></li>
+            </ul>
+          </div><!-- end col-sm-2 -->
+          
+          <div class="col-sm-2">
+            <h6 class="footerh6">Follow Us</h6>
+            <ul class="unstyled">
+              <li><a href="#">Twitter</a></li>
+              <li><a href="#">Facebook</a></li>
+              <li><a href="#">Google Plus</a></li>
+            </ul>
+          </div><!-- end col-sm-2 -->
+          
+          <div class="col-sm-2">
+            <h6 class="footerh6">Developed <span class="glyphicon glyphicon-user"></span> by Person's Name</h6>
+          </div><!-- end col-sm-2 -->
+        </div><!-- end row -->
+      </div><!-- end container -->
+      </footer>
+
+</div>
+  <!-- Include all compiled plugins (below), or include individual files as needed --> 
+  <!-- <script src="js/bootstrap.js"></script> -->
+  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.min.js"></script>
+  <script src="js/libs/bootstrap.min.js"></script>
+  <script src="js/script.js"></script>
+</body>
+</html>
