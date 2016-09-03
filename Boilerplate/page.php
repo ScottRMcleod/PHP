40d1a8c268d18754b91a7d3a2e18771618206eb7
Scott Mcleod <scott.mcleod@smcleodtech.com.au>
2016-09-03 12:07:46 +1000

    Create page.php

---
+++ Boilerplate/page.php
@@ -0,0 +1,159 @@
+<!--<script src="http://www.smcleodtech.com.au/js/libs/jquery-1.11.2.min.js"></script>-->
+<script>
+
+//JQuery inits the document for action once the page is loaded
+$(document).ready(function(){
+        $("#p_one").delay(100).hide(1000).hide().hide().fadeIn(2000);
+        $("#p_two").fadeOut(300).slideUp(1000).slideDown(1000);
+        $("#p_three").fadeOut(300).slideUp(1000).slideDown(1000);
+        $("#p_four").fadeOut(300).slideUp(1000).slideDown(1000);
+        $("#p_five").fadeOut(300).slideUp(1000).slideDown(1000);
+        $("#p_six").fadeOut(400).slideUp(1000).slideDown(1000);
+});
+</script>
+<div ng-view>
+            <div class="row" id="bigCallout">
+          <!-- Visible only on small devices -->
+          <div class="well well-small visible-sm">
+            <a href="" class="btn btn-large btn-block btn-info">
+              <span class="glyphicon glyphicon-phone"></span> Contact me here</a>
+          </div><!-- end well-small -->
+          <div class="well">
+            <div class="page-header">
+              <div class="panel panel-primary">
+              <div class="panel-heading">
+                <h1>Welcome to Scott Mcleod's Portfolio Site </h1>
+                </div>
+                <div class="panel-body">
+                  <div class="row">
+                  <center><img id = "p_one" class='smaller-image thin-gray-border'src='http://smcleodtech.com.au/Old_Version2/Images/blog.jpg' width='200px' height='200px'><br><br></center>
+                    </div>
+
+                    <div class="row">
+                      <div class="col-xs-12">
+                    <center><p>
+                        <h2>A highly skilled IT Professional / Consultant.</h2><br> 
+                      </p>
+                      <p>
+Often called to work with tight deadlines and offering solutions to complex problems, 
+being able to respond quickly and effectively to urgent cases has been the prime focus to manage high work volumes within large organisation.
+                    </p></center>
+                  </div>
+                  </div>
+                    <center>
+                    <div class="col-xs-12">
+                    <a href="#" class="progress-bar-primary progress-bar-striped btn btn-large btn-info">Mobile Apps</a>
+                    <a id ="" class="btn btn-large btn-success">Web Apps</a>
+                    <a id ="" href="" class="btn btn-large btn-warning" >Web Design</a>
+                    <a id ="" href="" class="btn btn-large btn-default" >Enterprise Applications</a>
+                    <a id ="" href="" class="btn btn-large btn-primary" >Product Training</a>
+                    <a id ="" href="" class="btn btn-large btn-danger" >Network Engineering</a>
+                </div></center>
+              </div>
+            </div><!-- end page-header -->    
+          </div><!-- end well -->
+        </div><!-- end col-12 -->
+        <!--this is the skills section of the page   -->
+<div class='panel panel-info'>
+  <!-- Default panel contents -->
+  <!--              this is the skills section of the page   -->
+  <div class='panel-heading'><center><h2>Skills</h2></center></div>
+  <div class='panel-body'>
+  <div class='row'> <!--Div1 Start Table rows <tr>-->
+    <div class='col-sm-8 col-lg-7'><!--Div2 Table coloms <td>Start-->
+      <div class='row'><!--Div3 Start new tr-->
+      <hr>
+      <div id="" class='col-xs-6'><!--Div4 Start-->
+      <div class='progress'><!--Div5 Start-->
+      <div class='progress-bar progress-bar-primary progress-bar-striped active' 
+           role='progressbar' aria-valuenow='85' 
+           aria-valuemin='0' 
+           aria-valuemax='100' 
+           style='width: 85%'> <!--Div6 HTML Start-->
+      Communication
+      </div><!--Div6 HTML End-->
+      </div><!--Div5 End-->
+      </div><!--Div4 End -->
+      <div id="p_nine" class='col-xs-6'><!--Div4 Start-->
+      <div class='progress'><!--Div5 Start-->
+      <div class='progress-bar 
+      progress-bar-success progress-bar-striped active' 
+      role='progressbar' aria-valuenow='85' 
+      aria-valuemin='0' 
+      aria-valuemax='100' 
+      style='width: 85%'> <!--Div6 HTML Start-->
+      Programming
+      </div><!--Div6 CSS End-->
+      </div><!--Div5 End-->
+      </div><!--Div4 End -->
+      </div><!--Div3 End -->
+    </div><!--Div2 End-->
+  </div><!--Div1 End-->
+  <div class='row'> <!--Div1 Start Table rows <tr>-->
+    <div class='col-sm-8 col-lg-7'><!--Div2 Table coloms <td>Start-->
+      <div class='row'><!--Div3 Start new tr-->
+          <hr>
+        <div id="" class='col-xs-6'><!--Div4 Start-->
+          <div class='progress'><!--Div5 Start-->
+                  <div class='progress-bar 
+              progress-bar-info progress-bar-striped active' 
+                role='progressbar' aria-valuenow='85' 
+              aria-valuemin='0' 
+              aria-valuemax='100' 
+              style='width: 85%'> <!--Div6 HTML Start-->
+              Learning
+            </div><!--Div6 HTML End-->
+          </div><!--Div5 End-->
+        </div><!--Div4 End -->
+        <div id=""class='col-xs-6'><!--Div4 Start-->
+          <div class='progress'><!--Div5 Start-->
+                  <div class='progress-bar 
+              progress-bar-warning progress-bar-striped active' 
+                role='progressbar' aria-valuenow='85' 
+              aria-valuemin='0' 
+              aria-valuemax='100' 
+              style='width: 45%'> <!--Div6 HTML Start-->
+              Visual Design
+            </div><!--Div6 HTML End-->
+          </div><!--Div5 End-->
+        </div><!--Div4 End -->
+      </div><!--Div3 End -->
+    </div><!--Div2 End-->
+  </div><!--Div1 End-->
+  <div class='row'> <!--Div1 Start Table rows <tr>-->
+    <div class='col-sm-8 col-lg-7'><!--Div2 Table coloms <td>Start-->
+      <div class='row'><!--Div3 Start new tr-->
+          <hr>
+        <div id=""class='col-xs-6'><!--Div4 Start-->
+          <div class='progress'><!--Div5 Start-->
+                  <div class='progress-bar progress-bar-danger
+              progress-bar-striped active' 
+                role='progressbar' aria-valuenow='85' 
+              aria-valuemin='0' 
+              aria-valuemax='100' 
+              style='width: 85%'> <!--Div6 HTML Start-->
+              Inforstructure Support
+            </div><!--Div6 HTML End-->
+          </div><!--Div5 End-->
+        </div><!--Div4 End -->
+        <div class='col-xs-6'><!--Div4 Start-->
+          <div class='progress'><!--Div5 Start-->
+                  <div class='progress-bar 
+              progress-bar-info progress-bar-striped active' 
+                role='progressbar' aria-valuenow='85' 
+              aria-valuemin='0' 
+              aria-valuemax='100' 
+              style='width: 85%'> <!--Div6 HTML Start-->
+              Database Development
+</div><!--Div6 CSS End-->
+</div><!--Div5 End-->
+</div><!--Div4 End -->
+</div><!--Div3 End -->
+</div><!--Div2 End-->
+</div><!--Div1 End-->
+</div>
+</div>
+</div><!-- end bigCallout -->
+</div>
+</div>
+ 
